78197604020201351246adaa07b29aa606fcc91d
P262362 <rmendozajmasncg@gmail.com>
2015-03-28 19:02:21 -0600

    Create servicio.php
    formulario de registro de nuevo usuario

---
+++ servicio.php
@@ -0,0 +1,24 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset= utf-8"/>
+
+<title>registro nuevo servicio</title>
+
+</head>
+
+<body>
+
+<form action="insertar.php" method="post" name="form">
+	
+USUARIO <input type="text" name="nombre"/> </br>
+CORREO <input type="text" name="correo"/> </br>
+CONTRASEÑA <input type="password" name="pw"/></br>
+        
+<input type="submit" value="guardar">
+
+</form>
+</body>
+</html>
