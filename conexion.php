661a44b73994749299569b753378e3630f84bd97
P262362 <rmendozajmasncg@gmail.com>
2015-03-28 19:00:37 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,6 +1,15 @@
+
+#cambiar datos de las variables por los datos de acceso a su servidor mysql
 <?php
+
+
 $host ="localhost";
+
 $user= "u225861_RAUL";
-$pw = "R946125M";
-$db = "u225861_PROYECTO_FINAL;
+
+$pw = "R946125M"; 
+
+$db = "u225861_PROYECTO_FINAL";
+
 ?>
+
