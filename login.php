d87940e3eda19e873fca0864eb3e5470df1537d2
P262362 <rmendozajmasncg@gmail.com>
2015-04-08 21:50:49 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,23 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset= utf-8"/>
+
+<title>registro nuevo servicio</title>
+
+</head>
+
+<body>
+<form action="verificar.php" method="post" name="form">
+	
+USUARIO <input type="text" name="nombre"/> </br>
+CONTRASEÑA <input type="password" name="pw"/></br>
+        
+<input type="submit" value="Ingresar">
+
+
+</form>
+</body>
+</html>
