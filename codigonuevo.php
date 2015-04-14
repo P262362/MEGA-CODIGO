6f22f6623e1c0b6d7e635d6cd6a11cc71aafc377
P262362 <rmendozajmasncg@gmail.com>
2015-04-14 14:05:12 -0500

    Create codigonuevo.php

---
+++ codigonuevo.php
@@ -0,0 +1,24 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset= utf-8"/>
+
+<title>NUEVO CODIGO</title>
+
+</head>
+
+<body>
+
+<form action="enviar_c.php" method="post">
+	
+Titulo <input type="text" name="asunto"/> </br>
+codigo <textarea name="mensaje"></textarea> </br>   
+Enviar por email al correo: <input type="text" name="correo"/> </br>    
+<input type="submit" value="enviar correo"/>
+
+</form>
+
+</body>
+
+</html>
