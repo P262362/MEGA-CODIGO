d8b29356fc2363ba9d634b037ff953b4d7053115
P262362 <rmendozajmasncg@gmail.com>
2015-04-19 19:53:41 -0500

    Update basedatos.sql

--- basedatos.sql
+++ basedatos.sql
@@ -1,20 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10.7
--- http://www.phpmyadmin.net
---
--- Servidor: localhost
--- Tiempo de generación: 26-03-2015 a las 20:47:01
--- Versión del servidor: 5.5.34-cll-lve
--- Versión de PHP: 5.4.23
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de datos: `u225861_PROYECTO_FINAL`
@@ -23,25 +10,13 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `CODIGO`
+-- Estructura de tabla para la tabla `codigo`
+--
+-- Creación: 19-04-2015 a las 05:48:41
 --
 
-CREATE TABLE IF NOT EXISTS `CODIGO` (
-  `USUARIO` varchar(50) NOT NULL,
-  `CODIGO` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `LOGIN`
---
-
-CREATE TABLE IF NOT EXISTS `LOGIN` (
-  `USUARIO` varchar(50) NOT NULL,
-  `PW` varchar(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE IF NOT EXISTS `codigo` (
+  `TITULO` text CHARACTER SET latin1 NOT NULL,
+  `USUARIO` text COLLATE utf8_spanish_ci NOT NULL,
+  `CODIGO` longtext COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
